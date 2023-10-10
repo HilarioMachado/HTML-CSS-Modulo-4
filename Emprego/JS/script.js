1c205f7672a449c76ad52f1d22c9e55be92a73ba
HilarioMachado <112648281+HilarioMachado@users.noreply.github.com>
2023-10-11 00:06:32 +0100

    Criei o menu

---
+++ Emprego/JS/script.js
@@ -0,0 +1,16 @@
+//JavaScript
+function menuOpen(){
+  if(itens.style.display == "flex"){
+    itens.style.display = "none";
+  }
+  else
+  itens.style.display = "flex";
+}
+
+function mudouTamanho(){
+    if(window.innerWidth > '768'){
+        itens.style.display = "flex";
+    }
+    else
+    itens.style.display = "none";
+}
